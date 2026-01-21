71d1832f5b156a6a24e9ce3beb36221e1a3e2d7e
oscmbsp07 <oscmbsp07@gmail.com>
2026-01-21 15:46:57 +0800

    Add app.py

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="Pemakluman Keputusan JKOSC", layout="wide")
+
+st.title("Pemakluman Keputusan JKOSC")
+st.info("App ini masih setup awal. Lepas ni kita akan buat fungsi transfer data ke template baru.")
